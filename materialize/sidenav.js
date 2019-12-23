f1be6a758f5f67b9aec19d91edb361b50ff3b452
Tristan <51232572+spozt@users.noreply.github.com>
2019-12-23 09:07:08 +0100

    Add files via upload

--- materialize/sidenav.js
+++ materialize/sidenav.js
@@ -1,3 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
   M.Sidenav.init(document.querySelectorAll('.sidenav'), {})
+  M.ScrollSpy.init(document.querySelectorAll('.scrollspy'), {})
+  M.Materialbox.init(document.querySelectorAll('.materialboxed'), {})
 })

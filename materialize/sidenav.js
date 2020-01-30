fd9f220fe3366b8a54c6f2d1b3b7919d6893fb13
Tristan <51232572+spozt@users.noreply.github.com>
2020-01-30 18:57:09 +0100

    Delete sidenav.js

--- materialize/sidenav.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  M.Sidenav.init(document.querySelectorAll('.sidenav'), {})
-  M.ScrollSpy.init(document.querySelectorAll('.scrollspy'), {})
-  M.Materialbox.init(document.querySelectorAll('.materialboxed'), {})
-})
